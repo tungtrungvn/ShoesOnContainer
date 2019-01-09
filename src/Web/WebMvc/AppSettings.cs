6d0fa378eafe0b8cc34ed96488a8c912b2321218
Huy Nguyen <huy.nguyen@act-on.net>
2019-01-08 16:16:05 -0800

    added the src files

---
+++ src/Web/WebMvc/AppSettings.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoesOnContainers.Web.WebMvc
+{
+    public class AppSettings
+    {
+      
+        public string CatalogUrl { get; set; }
+        public string CartUrl { get; set; }
+        public string OrderUrl { get; set; }
+        public Logging Logging { get; set; }
+    }
+
+    
+
+    public class Logging
+    {
+        public bool IncludeScopes { get; set; }
+        public Loglevel LogLevel { get; set; }
+    }
+
+    public class Loglevel
+    {
+        public string Default { get; set; }
+        public string System { get; set; }
+        public string Microsoft { get; set; }
+    }
+}
---
+++ src/Web/WebMvc/Infrastructure/ApiPaths.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoesOnContainers.Web.WebMvc.Infrastructure
+{
+    public class ApiPaths
+    {
+        public static class Basket
+        {
+            public static string GetBasket(string baseUri, string basketId)
+            {
+                return $"{baseUri}/{basketId}";
+            }
+
+            public static string UpdateBasket(string baseUri)
+            {
+                return baseUri;
+            }
+
+            public static string CleanBasket(string baseUri, string basketId)
+            {
+                return $"{baseUri}/{basketId}";
+            }
+        }
+
+        public static class Order
+        {
+            public static string GetOrder(string baseUri, string orderId)
+            {
+                return $"{baseUri}/{orderId}";
+            }
+
+            //public static string GetOrdersByUser(string baseUri, string userName)
+            //{
+            //    return $"{baseUri}/userOrders?userName={userName}";
+            //}
+            public static string GetOrders(string baseUri)
+            {
+                return baseUri;
+            }
+            public static string AddNewOrder(string baseUri)
+            {
+                return $"{baseUri}/new";
+            }
+        }
+
+        public static class Catalog
+        {
+            public static string GetAllCatalogItems(string baseUri, int page, int take, int? brand, int? type)
+            {
+                var filterQs = "";
+
+                if (brand.HasValue || type.HasValue)
+                {
+                    var brandQs = (brand.HasValue) ? brand.Value.ToString() : "null";
+                    var typeQs = (type.HasValue) ? type.Value.ToString() : "null";
+                    filterQs = $"/type/{typeQs}/brand/{brandQs}";
+                }
+
+                return $"{baseUri}items{filterQs}?pageIndex={page}&pageSize={take}";
+            }
+
+            public static string GetCatalogItem(string baseUri, int id)
+            {
+
+                return $"{baseUri}/items/{id}";
+            }
+            public static string GetAllBrands(string baseUri)
+            {
+                return $"{baseUri}catalogBrands";
+            }
+
+            public static string GetAllTypes(string baseUri)
+            {
+                return $"{baseUri}catalogTypes";
+            }
+        }
+
+
+    }
+}
